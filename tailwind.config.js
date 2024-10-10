4aeb03f10eba389a34fe29c4190538f89aad8e6f
Lucas <lucasbron@mailbox.org>
2024-10-10 05:34:45 +0200

    chore: unified and finalized header/main styling (incl. colors, menu items, breakpoint, font size, etc), replaced images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        neutral: {
+          800: "#1F1F1F",
+        },
+        "add-grey": "#292929",
+      },
       screens: {
-
+        // md: "820px",
+        // 'md-custom': '820px',
+        '3xl': '1820px',
+        '4xl': '1920px',
       },
     },
   },
